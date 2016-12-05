5c0ccf203598ff901c85c2750375f9370030fb14
Ethan McAuliffe <ethan@mcauliffe-657.local>
2016-12-05 09:16:35 -0500

    Added comments and made the function more efficient if the jump cannot be made

--- 7-13-Mario/Mario.playground/Contents.swift
+++ 7-13-Mario/Mario.playground/Contents.swift
@@ -16,18 +16,31 @@ import Foundation
 func levelCost(heights: [Int], maxJump: Int) ->Int{
 	var energy:Int = 0
 	var jumpHeight:Int = 0
+	
+	//Iterate over all the heights
 	for i in 0...(heights.count-2){
+		
+		//What is the jump height at this point?
+		//Difference between current (i) and next (i+1)
 		jumpHeight = heights[i+1]-heights[i]
+		
+		//Is the jumpHeiht "jumplable"?
 		if jumpHeight <= maxJump{
+			
+			//We cab di tge jump, so...
 			if jumpHeight==0{
+				//Jumping across to another platform at same level
 				energy += 1
 			} else {
+				//Add the energy taken to make this jump
 				energy+=abs(jumpHeight*2)
 			}
 		} else {
-			energy = -1
-			break
+			//Can't do this jump
+			return -1
 		}
 	}
+	
+	//Returning the total engergy consumed to complete the level
 	return energy
 }
