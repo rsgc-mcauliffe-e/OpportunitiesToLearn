b4017352194da49cd0823589c3645f42df02f7e7
Ethan McAuliffe <ethan@mcauliffe-657.local>
2017-01-22 01:03:27 -0500

    improved realism of frog motion

--- CustomAnimation.playground/Contents.swift
+++ CustomAnimation.playground/Contents.swift
@@ -15,7 +15,7 @@ let bill = SKSpriteNode(imageNamed: "hqdefault")
 let frog = SKSpriteNode(imageNamed: "forg.png")
 bground.position = midPoint
 bground.zPosition = 0 //ensures the background is the furthest back object
-frog.position = CGPoint(x: 325, y: 10)
+frog.position = CGPoint(x: 400, y: 10)
 bill.position = midPoint
 frog.setScale(0.3)
 bill.setScale(0.3)
@@ -30,10 +30,14 @@ let actionSequence = SKAction.sequence([actionMoveUp, actionMoveDown, rotate])
 let actionRepeat = SKAction.repeatForever(actionSequence)
 
 
-let actionFly = SKAction.moveBy(x: -400, y: 400, duration: 7)
+let actionFlyUp = SKAction.moveBy(x: -400, y: 200, duration: 7)
+let actionFlyRight = SKAction.moveBy(x: 275, y: 0, duration: 1)
+let actionFlyDown = SKAction.moveBy(x: 0, y: -250, duration: 10)
+let actionHome = SKAction.move(to: CGPoint(x: 400, y: 10), duration: 0)
+let actionFlyAroundForever = SKAction.repeatForever(SKAction.sequence([actionFlyUp, wait, actionFlyRight, actionFlyDown, actionHome]))
 
 bill.run(actionRepeat)
-frog.run(actionFly)
+frog.run(actionFlyAroundForever)
 bill.zPosition = 2  // Ensure sprite is above background and frog
 
 scene.addChild(bill) // Add to the scene
